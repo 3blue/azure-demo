2127de07d9e5e0de8e244a067b88f1031c57ef23
Evan McKinnon <evan.mckinnon.1@gmail.com>
2022-06-08 16:36:18 -0400

    pyaudio workaround

--- hello_app/views.py
+++ hello_app/views.py
@@ -11,7 +11,7 @@ def home():
         #response = query(request.form['question'], request.form['context'])
         #response = ''.join( c for c in response if  c not in '[]",' )
         #out = "THE MODEL SAYS: " + response
-        return render_template("home.html") + listen_to_mic
+        return render_template("home.html") + listen_to_mic()
     return render_template("home.html")
 
 @app.route("/about/")
@@ -54,14 +54,37 @@ def query(question, context):
     data = json.dumps(data)
     resp = requests.post(scoring_uri, data=data, headers=headers)
     return resp.text
-    
+
+class DuckTypedMicrophone( speech_recognition.AudioSource ): # descent from AudioSource is required purely to pass an assertion in Recognizer.listen()
+    def __init__( self, device=None, chunkSeconds=1024/44100.0 ):  # 1024 samples at 44100 Hz is about 23 ms
+        self.recorder = None
+        self.device = device
+        self.chunkSeconds = chunkSeconds
+    def __enter__( self ):
+        self.nSamplesRead = 0
+        self.recorder = audiomath.Recorder( audiomath.Sound( 5, nChannels=1 ), loop=True, device=self.device )
+        # Attributes required by Recognizer.listen():
+        self.CHUNK = audiomath.SecondsToSamples( self.chunkSeconds, self.recorder.fs, int )
+        self.SAMPLE_RATE = int( self.recorder.fs )
+        self.SAMPLE_WIDTH = self.recorder.sound.nbytes
+        return self
+    def __exit__( self, *blx ):
+        self.recorder.Stop()
+        self.recorder = None
+    def read( self, nSamples ):
+        sampleArray = self.recorder.ReadSamples( self.nSamplesRead, nSamples )
+        self.nSamplesRead += nSamples
+        return self.recorder.sound.dat2str( sampleArray )
+    @property
+    def stream( self ): # attribute must be present to pass an assertion in Recognizer.listen(), and its value must have a .read() method
+        return self if self.recorder else None
+        
 def listen_to_mic():
     r = sr.Recognizer()
-    with sr.Microphone() as source:
-        #print("Speak Anything :")
+    with DuckTypedMicrophone() as source:
         audio = r.listen(source)
         try:
             text = r.recognize_google(audio)
             return "You said : {}".format(text)
         except:
-            return "Sorry could not recognize what you said"
+            return "Sorry, could not recognize what you said."
