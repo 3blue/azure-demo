96a4130f2d04ffdcc88e4aa93ea0920b820dbac2
Evan McKinnon <evan.mckinnon.1@gmail.com>
2022-06-08 22:17:21 -0400

    ya

--- hello_app/views.py
+++ hello_app/views.py
@@ -4,7 +4,6 @@ from datetime import datetime
 from flask import Flask, render_template, request
 from . import app
 import speech_recognition as sr
-import audiomath; audiomath.RequireAudiomathVersion( '1.12.0' )
 
 @app.route("/", methods=['GET', 'POST'])
 def home():
@@ -55,34 +54,10 @@ def query(question, context):
     data = json.dumps(data)
     resp = requests.post(scoring_uri, data=data, headers=headers)
     return resp.text
-
-class DuckTypedMicrophone( sr.AudioSource ): # descent from AudioSource is required purely to pass an assertion in Recognizer.listen()
-    def __init__( self, device=None, chunkSeconds=1024/44100.0 ):  # 1024 samples at 44100 Hz is about 23 ms
-        self.recorder = None
-        self.device = device
-        self.chunkSeconds = chunkSeconds
-    def __enter__( self ):
-        self.nSamplesRead = 0
-        self.recorder = audiomath.Recorder( audiomath.Sound( 5, nChannels=1 ), loop=True, device=self.device )
-        # Attributes required by Recognizer.listen():
-        self.CHUNK = audiomath.SecondsToSamples( self.chunkSeconds, self.recorder.fs, int )
-        self.SAMPLE_RATE = int( self.recorder.fs )
-        self.SAMPLE_WIDTH = self.recorder.sound.nbytes
-        return self
-    def __exit__( self, *blx ):
-        self.recorder.Stop()
-        self.recorder = None
-    def read( self, nSamples ):
-        sampleArray = self.recorder.ReadSamples( self.nSamplesRead, nSamples )
-        self.nSamplesRead += nSamples
-        return self.recorder.sound.dat2str( sampleArray )
-    @property
-    def stream( self ): # attribute must be present to pass an assertion in Recognizer.listen(), and its value must have a .read() method
-        return self if self.recorder else None
         
 def listen_to_mic():
     r = sr.Recognizer()
-    with DuckTypedMicrophone() as source:
+    with sr.Microphone() as source:
         audio = r.listen(source)
         try:
             text = r.recognize_google(audio)
